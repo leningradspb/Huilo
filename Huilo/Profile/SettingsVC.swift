92dd9d8cd0140b10f58f29478ff49df7ce85eb17
ed <edsiniakov@aol.com>
2022-12-30 21:51:01 +0300

    settings fixes

--- Huilo/Profile/SettingsVC.swift
+++ Huilo/Profile/SettingsVC.swift
@@ -6,16 +6,26 @@
 //
 
 import UIKit
+import SafariServices
 
 class SettingsVC: GradientVC {
-    private let logOutButton = VioletButton(text: "sing out 👋")
-    private let deleteAccountButton = VioletButton(text: "delete account 🥹")
+    private let privacyButton = VioletButton(text: "privacy policy 🔐")
+    private let logOutButton = VioletButton(text: "sign out 👋")
+    private let deleteAccountButton = VioletButton(text: "delete account 😰")
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        gradientContentView.addSubviews([logOutButton, deleteAccountButton])
+        setupNavigationBar(with: "settings")
+        gradientContentView.addSubviews([logOutButton, deleteAccountButton, privacyButton])
+        privacyButton.addTarget(self, action: #selector(privacyTapped), for: .touchUpInside)
         logOutButton.addTarget(self, action: #selector(singOut), for: .touchUpInside)
         deleteAccountButton.addTarget(self, action: #selector(deleteAccount), for: .touchUpInside)
+        
+        privacyButton.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(Layout.leading)
+            $0.trailing.equalToSuperview().offset(-Layout.leading)
+            $0.bottom.equalTo(logOutButton.safeAreaLayoutGuide.snp.top).offset(-6)
+        }
         
         logOutButton.snp.makeConstraints {
             $0.leading.equalToSuperview().offset(Layout.leading)
@@ -58,6 +68,14 @@ class SettingsVC: GradientVC {
                 self.present(vc, animated: false, completion: nil)
             }
         })
+    }
+    
+    @objc private func privacyTapped() {
+        let vc = SFSafariViewController(url: URL(string: "https://docs.google.com/document/d/1G4iy8Xom40GM0OzkmjRraas4Xbd4AW-wTmv0_fryeO8/edit?usp=sharing")!)
+        
+        vc.isModalInPresentation = true
+        vc.modalPresentationStyle = .overFullScreen
+        present(vc, animated: true, completion: nil)
     }
 
 }
