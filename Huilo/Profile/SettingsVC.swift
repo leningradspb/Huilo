e6e8d5f284697e727cdc0ebb0c2dedff338d02b2
Ed <Siniakov>
2022-12-30 18:52:05 +0300

    delete account added

--- Huilo/Profile/SettingsVC.swift
+++ Huilo/Profile/SettingsVC.swift
@@ -8,14 +8,22 @@
 import UIKit
 
 class SettingsVC: GradientVC {
-    private let logOutButton = VioletButton(text: "Sing out")
+    private let logOutButton = VioletButton(text: "sing out 👋")
+    private let deleteAccountButton = VioletButton(text: "delete account 🥹")
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        gradientContentView.addSubviews([logOutButton])
+        gradientContentView.addSubviews([logOutButton, deleteAccountButton])
         logOutButton.addTarget(self, action: #selector(singOut), for: .touchUpInside)
+        deleteAccountButton.addTarget(self, action: #selector(deleteAccount), for: .touchUpInside)
         
         logOutButton.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(Layout.leading)
+            $0.trailing.equalToSuperview().offset(-Layout.leading)
+            $0.bottom.equalTo(deleteAccountButton.safeAreaLayoutGuide.snp.top).offset(-6)
+        }
+        
+        deleteAccountButton.snp.makeConstraints {
             $0.leading.equalToSuperview().offset(Layout.leading)
             $0.trailing.equalToSuperview().offset(-Layout.leading)
             $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).offset(-6)
@@ -34,6 +42,22 @@ class SettingsVC: GradientVC {
         } catch let signOutError as NSError {
           print("Error signing out: %@", signOutError)
         }
+    }
+    
+    @objc private func deleteAccount() {
+        FirebaseManager.shared.auth.currentUser?.delete(completion: { [weak self] error in
+            guard let self = self else { return }
+            if let error = error {
+                self.view.showMessage(text: error.localizedDescription, isError: true)
+            } else {
+                let vc = AuthVC()
+                vc.setupMainUI = { [weak self] in
+                    guard let self = self else { return }
+                }
+                vc.modalPresentationStyle = .fullScreen
+                self.present(vc, animated: false, completion: nil)
+            }
+        })
     }
 
 }
